4983b2d3da05d2d1e9f72280c09a90df1d579fc2
Sakthikumar1507 <sakthikumarsoundarrajan@gmail.com>
2024-07-17 11:02:23 +0530

    change

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,url_for,redirect,session,request,flash
+from flask_mail import Mail,Message
+
+app=Flask(__name__,static_folder="static")
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if 'calc' in request.form:
+        if request.method=="POST":
+            
+            fname=request.form["fname"]
+            lname=request.form["lname"]
+            
+            app.config['MAIL_SERVER']='smtp.gmail.com'  
+            app.config['MAIL_PORT']=465  
+            app.config['MAIL_USERNAME'] ='sportsclub3115@gmail.com'
+            app.config['MAIL_PASSWORD'] ='lkbq ewpe spbz qsya'
+            app.config['MAIL_USE_TLS'] = False  
+            app.config['MAIL_USE_SSL'] = True  
+            mail=Mail(app) 
+
+            msg=Message('WELCOME TO SPORTSCLUB TURF UNIVERSE',sender='sportsclub3115@gmail.com',recipients=['kumar6603517@gmail.com'])    
+            msg.body=fname + lname
+                         
+                         
+            mail.send(msg)
+            return redirect(url_for("love"))
+            
+            
+    return render_template("home.html") 
+    
+    
+@app.route("/love",methods=["GET","POST"])
+def love():
+      
+    
+    
+    
+    
+      return render_template("show.html")
+    
+if(__name__=='__main__'):
+     app.secret_key="flask"
+     app.run(debug=True)
